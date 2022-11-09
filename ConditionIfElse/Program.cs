ae5c20da7feaddb6456837e87a1411882bdc26e8
Vanger <stepan13@gmail.com>
2022-11-09 17:35:27 +0000

    ifelse

---
+++ ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Это маша");
+}
+else
+{
+    Console.WriteLine("Это не маша: ");
+    Console.WriteLine(username);
+}
