4a2c0345f83343cd3c63f2998eb6f130ef09a1ee
Vanger <stepan13@gmail.com>
2022-11-09 17:26:58 +0000

    random

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿//Console.Write("Введите первое число: ");
-int num1 = 3;
-int num2 = 4;
+int num1 = new Random().Next(1, 10);//1 2 3 ...9
+Console.WriteLine(num1);
+int num2 = new Random().Next(1, 10);//1 2 3 ...9
+Console.WriteLine(num2);
 int result = num1+num2;
 //Console.Write("Введите второе число: ");
 Console.WriteLine(result);
